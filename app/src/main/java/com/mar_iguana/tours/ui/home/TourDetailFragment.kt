5fe9bbb906669959f443d9d2de5062a0de9f432e
Edgar <edgar.zamora.aranda@gmail.com>
2021-04-11 00:04:37 -0500

    Added book button

--- app/src/main/java/com/mar_iguana/tours/ui/home/TourDetailFragment.kt
+++ app/src/main/java/com/mar_iguana/tours/ui/home/TourDetailFragment.kt
@@ -5,17 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import com.mar_iguana.tours.R
 import com.mar_iguana.tours.adapters.PagerTabAdapter
 import com.mar_iguana.tours.databinding.FragmentTourDetailBinding
 import com.mar_iguana.tours.models.Tour
-
+import com.mar_iguana.tours.ui.home.book.BookFragment
 
 class TourDetailFragment : Fragment() {
 
-    lateinit var imageViewPager : ViewPager2
     private var _binding: FragmentTourDetailBinding? = null
     private val binding get() = _binding!!
 
@@ -26,17 +24,26 @@ class TourDetailFragment : Fragment() {
 
         //Get data Tour
         val tourDetail =  arguments?.getParcelable<Tour>("dataTour")
+        initViewPager()
+        
+        binding.buttonBook.setOnClickListener {
+            parentFragmentManager.beginTransaction().apply {
+                replace(R.id.nav_host_fragment, BookFragment.newInstance(tourDetail!!))
+                commit()
+            }
+        }
 
-        tourDetail?.images?.get(0)?.let { binding.appBarImage.setImageResource(it) }
-
-        initViewPager(view, tourDetail)
         return view
     }
 
     //Configure tabLayout and ViewPager to show detail
-    private fun initViewPager(view:View, tour: Tour?){
+    private fun initViewPager(){
+        //Get data Tour
+        val tourDetail =  arguments?.getParcelable<Tour>("dataTour")
+        tourDetail?.images?.get(0)?.let { binding.appBarImage.setImageResource(it) }
+
         val adapterPager = PagerTabAdapter(parentFragmentManager , lifecycle)
-        tour?.let { adapterPager.setTour(it) }
+        tourDetail?.let { adapterPager.setTour(it) }
         binding.pagerDetailTour.adapter = adapterPager
 
         val tabTitles: ArrayList<String> = arrayListOf(getString(R.string.tab_info), getString(R.string.tab_itinerary))
